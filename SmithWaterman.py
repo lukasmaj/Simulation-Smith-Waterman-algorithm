d7984c16a4b04b342363366313ca53e3ba152d7c
Lukasz Majkowski <lukasz.majkowski.gm@gmail.com>
2020-02-15 00:32:39 +0100

    SW-alg Done

--- SmithWaterman.py
+++ SmithWaterman.py
@@ -27,20 +27,74 @@ class SmithWatermanAlgorithm:
     def scoringLeft(self, scoringMatrix, n, m, Wk):
         ret = scoringMatrix[m][n-1]-Wk
         return ret
+
     def scoringCross(self, scoringMatrix, n, m, AImage, BImage):
         ret = scoringMatrix[m-1][n-1] + self.score(a=AImage[n], b=BImage[m])
         return ret
+
+    def computeMaxScore(self,scoringMatrix):
+        maxScore = 0
+        n, m = len(scoringMatrix[0]), len(scoringMatrix)
+        for i in range(0, m):
+            for j in range(0, n):
+                if scoringMatrix[i][j] > maxScore:
+                    maxScore = scoringMatrix[i][j]
+                    posA = j
+                    posB = i
+        return posA, posB
+
+    def computeBacktrack(self, scoringMatrix, maxScore):
+        posA, posB = maxScore
+        seqence =[]
+
+        while posA != 1 and posB != 1:
+            up = scoringMatrix[posB-1][posA]
+            left = scoringMatrix[posB-1][posA]
+            cross = scoringMatrix[posB-1][posA-1]
+            maxNexScore = max(up, left, cross)
+
+            if cross == maxNexScore or (posA-1 == 1) or (posB == 1):
+                seqence.append([posA, posB])
+                posA = posA -1
+                posB = posB -1
+            elif up == maxNexScore:
+                seqence.append([None, posB])
+                posA = posA
+                posB = posB - 1
+            elif left == maxNexScore:
+                seqence.append([posA, None])
+                posA = posA -1
+                posB = posB
+        return seqence
 
     def score(self, a, b):
         if a == b:
             return 3
         else:
             return -3
+    def exposeSequence(self, sequence, AImage, BImage):
+        stra=""
+        strb=""
+        for s in sequence:
+            a, b = s
+            if a is not None:
+                stra = stra + "{0}".format(AImage[a])
+            else:
+                stra = stra + "-"
+            if b is not None:
+                strb = strb + "{0}".format(BImage[b])
+            else:
+                strb = strb + "-"
+        print stra[::-1]
+        print strb[::-1]
 
     def alignSequence(self, AImage, BImage):
         print "Align Sequence"
-        ret = self.fillScoringMatrix(AImage=AImage, BImage=BImage)
-        utils.exposeMatrix(ret)
+        scoringMatrix = self.fillScoringMatrix(AImage=AImage, BImage=BImage)
+        maxScore = self.computeMaxScore(scoringMatrix=scoringMatrix)
+        findSequence = self.computeBacktrack(scoringMatrix=scoringMatrix, maxScore=maxScore)
+        self.exposeSequence(sequence=findSequence, AImage=AImage, BImage=BImage)
+        # utils.exposeMatrix(scoringMatrix)
 
 __smAlgorithm = SmithWatermanAlgorithm()
 alignSequence = __smAlgorithm.alignSequence
